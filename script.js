dc7c3f7da51ba7ebcf1864e1b851c7c0dc811fc6
vincentt14 <75069457+vincentt14@users.noreply.github.com>
2021-12-14 12:35:44 +0700

    tampilan kelar, belum media query sama javascript

---
+++ script.js
@@ -0,0 +1,19 @@
+// const foto1 = document.querySelector('.foto1');
+// const content = document.querySelector('.foto1 .content');
+// foto1.addEventListener('mouseover',function(){
+//     content.style.display = 'block';
+//     foto1.classList.add('overlay');
+// });
+
+// foto1.addEventListener('mouseout',function(){
+//     content.style.display = 'none';
+// });
+
+const nama = document.getElementsByName('nama');
+const email = document.getElementsByName('_replyto');
+const nomor = document.getElementsByName('nomor');
+const pesan = document.getElementsByName('message');
+
+const tombol = document.querySelector('.contactform form .btn');
+
+nama.values
